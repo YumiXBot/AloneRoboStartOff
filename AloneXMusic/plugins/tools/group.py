8ca29c2c4285ddc637420132b885a8b50880b3b6
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-12-11 14:34:55 +0530

    Update group.py

--- AloneXMusic/plugins/tools/group.py
+++ AloneXMusic/plugins/tools/group.py
@@ -11,22 +11,6 @@ async def brah(_, msg):
 async def brah2(_, msg):
        await msg.reply("𝐕ᴏɪᴄᴇ 𝐂ʜᴀᴛ 𝐄ɴᴅᴇᴅ")
 
-# invite members on vc
-@app.on_message(filters.video_chat_members_invited)
-async def brah3(app :app, message:Message):
-           text = f"{message.from_user.mention} 𝐈ɴᴠɪᴛᴇᴅ "
-           x = 0
-           for user in message.video_chat_members_invited.users:
-             try:
-               text += f"[{user.first_name}](tg://user?id={user.id}) "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text} 😉")
-           except:
-             pass
-
 
 ####
 
