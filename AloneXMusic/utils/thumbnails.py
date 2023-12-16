874980350bb3f9d479eda7a1ecd699c90e648c8b
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-12-17 00:42:09 +0530

    Update thumbnails.py

--- AloneXMusic/utils/thumbnails.py
+++ AloneXMusic/utils/thumbnails.py
@@ -107,7 +107,7 @@ async def get_thumb(videoid):
         )
         draw.text(
             (600, 200),
-            f"NOW PLAYING",
+            f"ALONE PLAYING",
             fill="white",
             stroke_width=2,
             stroke_fill="yellow",
